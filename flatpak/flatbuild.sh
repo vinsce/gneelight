c57c9e6258b2993d3454d48f49f4fa9c08b8dbed
Vincenzo Cerminara <vincenzo.cerminara94@gmail.com>
2021-11-15 00:46:19 +0100

    fix custom build script in github workflow

--- flatpak/flatbuild.sh
+++ flatpak/flatbuild.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-flatpak-builder --repo=./../repo -v --force-clean --install-deps-from=flathub build-dir me.vinsce.Gneelight.json
+flatpak-builder --repo=./../repo -v --force-clean build-dir me.vinsce.Gneelight.json
 flatpak build-bundle ./../repo gneelight.flatpak me.vinsce.Gneelight

c432f86f299246292c2100914ddcf637dd64566e
Vincenzo Cerminara <vincenzo.cerminara94@gmail.com>
2021-11-16 19:42:44 +0100

    Add GitHub workflow to build Flatpak package, update Gnome (#10)
    - Add GitHub workflow to build flatpak package
    - Update to Gnome 3.40
    - Using flatpak shared-modules

--- src/constants.py
+++ src/constants.py
@@ -7,4 +7,4 @@ APP_ICON = os.path.dirname(inspect.stack()[0][1]) + "/../res/gneelight.png"
 AUTHORS = ['Vincenzo Cerminara']
 WEBSITE = 'https://github.com/vinsce/gneelight'
 APP_COMMENTS = 'A Yeelight controller for GNOME'
-APP_VERSION = '1.0.4'
+APP_VERSION = '1.0.5'
